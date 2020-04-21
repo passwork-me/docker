bdcce20c2d9fa19645b286c8cdb809eefd6ebac1
Konstantin Nemanov <nemanov.konstantin@yandex.ru>
2020-04-21 09:28:02 +0000

    Commented cookie secure in php.ini; Auto-copy of config.ini; Auto-replace from localhost to db in the config.ini

--- docker-compose/nginx/docker-entrypoint.sh
+++ docker-compose/nginx/docker-entrypoint.sh
@@ -2,5 +2,7 @@
 
 chown -R www-data:adm /server/log/nginx/
 chown -R www-data:www-data /server/sites/
+cp /server/sites/prod/app/config/config.example.ini /server/sites/prod/app/config/config.ini
+sed -i 's/localhost:/db:/g' '/server/sites/prod/app/config/config.ini'
 
 exec "$@"
